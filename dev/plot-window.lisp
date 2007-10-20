0989de3309da1c2dc1b816629aefdc5be7bfd8d9
Gary King <gwking@metabang.com>
2007-10-19 22:45:57 -0400

    Use trivial-shell instead of digitool specific stuff
    darcs-hash:20071020024557-3cc5d-236efec7336131a82771d7cda5e2a443b5011525.gz

--- dev/plot-window.lisp
+++ dev/plot-window.lisp
@@ -6,22 +6,18 @@
   (write-plot plot :postscript)
   (execute-plot plot))
 
-;;; ---------------------------------------------------------------------------
-  
 (defun execute-plot (plot) 
   (if (probe-file (fullpath plot))
-    (ccl::do-shell-script
+    (trivial-shell:shell-command 
      (format nil "export PATH=/usr/local/bin:$PATH; cd ~A; ~A ~A"
-             (macintosh-path->unix (make-pathname :type nil 
-                                                  :name nil
-                                                  :defaults (fullpath plot)))
+             (make-pathname :type nil 
+			    :name nil
+			    :defaults (fullpath plot))
              *gnuplot-home*
-             (make-pathname :name (pathname-name (fullpath plot))
-                            :type (pathname-type (fullpath plot)))))
+             (pathname-name+type (fullpath plot))))
     (error "Plot command file ~A does not exist" (fullpath plot))))
 
-;;; ---------------------------------------------------------------------------
-
+#+digitool
 (defmethod open-plot-in-window (plot)
   (write-plot plot :pdf)
   (let ((pdf (make-pathname :type "pdf"
